abbf4220099f813bcb55f698485154f97887c86c
shuva9800 <ffshuva@gmail.com>
2024-04-16 10:31:43 +0530

    minor style change

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const navbar = document.querySelector(".hero-section-header");
 const paraStyle = document.querySelector(".langchain");
+const header = document.querySelector(".hero-section-header");
 window.addEventListener('scroll', ()=> {
 
     if (window.scrollY > 0) { // Adjust the scroll threshold as needed
       navbar.classList.add('scrolled');
-      paraStyle.classList.add('active');
+      paraStyle.classList.add('activecolor');
+      header.classList.add('activebg');
     } else {
       navbar.classList.remove('scrolled');
-      paraStyle.classList.remove('active')
+      paraStyle.classList.remove('activecolor');
+      header.classList.add('activebg');
     }
   });
